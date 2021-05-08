cd18604b01ab5ed327a74b32f8ac6ef50b6cc31a
slowdolphin <tighe59@gmail.com>
2021-05-08 14:46:53 -0400

    onto the 2nd set of tasks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //task 1.0
 const mainEl = document.querySelector('main');
-
+//task 2.0
+const topMenuEl = document.querySelector('top-menu');
 
 //task 1.1
 mainEl.style.backgroundColor = 'var(--main-bg)';
@@ -8,3 +9,9 @@ mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1> SEI ROCKS! </h1>'
 //task 1.3
 mainEl.classList.add('flex-ctr')
+
+//task 2.1
+
+//task 2.2
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+//task 2.3
