96687e91c2b09b158f57338e77636ba986fcf1c4
slowdolphin <tighe59@gmail.com>
2021-05-08 15:49:06 -0400

    what is due is done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
+//Task 3.0
+// Menu data structure
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '/catalog'},
+  {text: 'orders', href: '/orders'},
+  {text: 'account', href: '/account'},
+];
+
+
 //task 1.0
 const mainEl = document.querySelector('main');
 //task 2.0
-const topMenuEl = document.querySelector('top-menu');
+const topMenuEl = document.querySelector('#top-menu');
 
 //task 1.1
 mainEl.style.backgroundColor = 'var(--main-bg)';
@@ -9,9 +19,26 @@ mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1> SEI ROCKS! </h1>'
 //task 1.3
 mainEl.classList.add('flex-ctr')
-
 //task 2.1
-
+topMenuEl.style.height = '100%';
 //task 2.2
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 //task 2.3
+topMenuEl.classList.add('flex-around');
+
+//Task 3.1
+
+// for in loop: for (const property in object) {
+//   console.log(`${property}: ${object[property]}`);
+// }
+
+
+
+for(let menulink of menuLinks){
+
+  const link = document.createElement('a');
+  link.textContent = menulink.text
+  link.setAttribute('href', menulink.href)
+  topMenuEl.appendChild(link)
+  // console.log(topMenuEl)
+}
