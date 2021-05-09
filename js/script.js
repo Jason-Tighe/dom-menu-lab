6decdfcdff36eb27c152b223d8d9d246d95bc29e
slowdolphin <tighe59@gmail.com>
2021-05-09 16:28:45 -0400

    working on part 2 updates

--- js/script.js
+++ js/script.js
@@ -12,6 +12,8 @@ var menuLinks = [
 const mainEl = document.querySelector('main');
 //task 2.0
 const topMenuEl = document.querySelector('#top-menu');
+//task 4.0
+const subMenuEl = document.querySelector('#sub-menu');
 
 //task 1.1
 mainEl.style.backgroundColor = 'var(--main-bg)';
@@ -25,15 +27,23 @@ topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 //task 2.3
 topMenuEl.classList.add('flex-around');
+//task 4.1
+subMenuEl.style.height = '100%';
+//task 4.2
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+//task 4.3
+subMenuEl.classList.add ('flex-around');
+//task 4.4
+subMenuEl.style.position = 'absolute';
+
+//task 4.5
+subMenuEl.style.top = 0;
 
 //Task 3.1
 
 // for in loop: for (const property in object) {
 //   console.log(`${property}: ${object[property]}`);
 // }
-
-
-
 for(let menulink of menuLinks){
 
   const link = document.createElement('a');
